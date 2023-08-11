505197e938118d47177dfd65981f87d56eced616
Tim Benniks <tbenniks@gmail.com>
2023-08-11 10:53:39 +0200

    moar better images

--- components/CloudinaryImage.vue
+++ components/CloudinaryImage.vue
@@ -1,5 +1,23 @@
 <script setup lang="ts">
 defineProps(["image", "alt", "loading", "basewidth"]);
+
+function createSourceSet(image: any, w: number, h: number) {
+  let srcset = "";
+
+  [
+    300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600,
+    1700,
+  ].forEach((width) => {
+    const url = image.secure_url.replace(
+      `v${image.version}`,
+      `q_auto,f_auto/c_limit,w_${width},ar_${w.toFixed(0)}:${h.toFixed(0)}`
+    );
+
+    srcset += `${url} ${width}w, `;
+  });
+
+  return srcset.slice(0, -2);
+}
 </script>
 
 <template>
@@ -8,7 +26,7 @@ defineProps(["image", "alt", "loading", "basewidth"]);
       :src="
         image.secure_url.replace(
           `v${image.version}`,
-          'q_auto:low,f_auto/c_limit,w_auto'
+          'q_auto:low,f_auto/c_limit,w_500'
         )
       "
       :alt="alt || image?.context?.custom?.alt || ''"
@@ -16,6 +34,13 @@ defineProps(["image", "alt", "loading", "basewidth"]);
       :height="(basewidth || 960) * (image.height / image.width)"
       class="fancy-image w-full block"
       :loading="loading || 'lazy'"
+      :srcset="
+        createSourceSet(
+          image,
+          basewidth || 960,
+          (basewidth || 960) * (image.height / image.width)
+        )
+      "
     />
   </div>
 </template>
