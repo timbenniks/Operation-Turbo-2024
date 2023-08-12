c7d6d819b97b5822357784010ac052d306dc407a
Tim Benniks <tbenniks@gmail.com>
2023-08-12 12:13:02 +0200

    latest content

--- components/Blocks/RichTextBlock.vue
+++ components/Blocks/RichTextBlock.vue
@@ -6,7 +6,7 @@ defineProps(["id", "componentName", "space", "copy"]);
   <div :class="space ? 'mb-8' : 'mb-4'">
     <div
       v-html="copy.html"
-      class="prose prose-h3:text-slate-200 prose-h3:text-3xl prose-h3:mb-4 prose-p:md-4 max-w-4xl prose-p:text-slate-400 prose-a:text-white prose-a:no-underline hover:prose-a:text-slate-400 hover:prose-a:underline"
+      class="prose prose-h2:text-slate-200 prose-h2:text-3xl prose-h2:font-medium prose-h3:text-slate-200 prose-h3:text-3xl prose-h3:mb-4 prose-p:md-4 max-w-4xl prose-p:text-slate-400 prose-a:text-white prose-a:no-underline hover:prose-a:text-slate-400 hover:prose-a:underline"
     />
   </div>
 </template>
--- pages/alive-and-kicking.vue
+++ pages/alive-and-kicking.vue
@@ -6,10 +6,18 @@ await useMetaData(slug);
 </script>
 
 <template>
-  <div class="mt-4 ml-4 lg:mt-12 lg:ml-12 max-w-[900px]">
+  <div class="mt-4 ml-4 lg:mt-12 lg:ml-12 max-w-[1280px]">
     <main class="pr-4 md:pr-12 pt-6">
       <page-header :title="title" :slug="slug" />
       <blocks-list :slug="slug" />
+
+      <videos-list
+        playlist-id="PLcoeeDyxakhUdkUvZm8qld1YuInOKOLqT"
+        :per-page="3"
+        title="The story begind Alive and kicking"
+        see-all-link="https://www.youtube.com/@timbenniks"
+        :show-per-page="false"
+      />
     </main>
   </div>
 </template>
