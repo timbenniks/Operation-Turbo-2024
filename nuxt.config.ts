3e40cd1b84f1b7464a0bfa81baf1bde3459d8c5f
Tim Benniks <tbenniks@gmail.com>
2023-08-12 13:13:41 +0200

    back to prerendering to not exhaust my YT API key

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,24 +3,24 @@ export default defineNuxtConfig({
     //componentIslands: true
     noScripts: true
   },
-  // nitro: {
-  //   prerender: {
-  //     crawlLinks: true,
-  //     interval: 1000,
-  //     concurrency: 1
-  //   }
-  // },
-
-  routeRules: {
-    '/': { isr: true },
-    '/about': { isr: true },
-    '/videos': { isr: true },
-    '/writing': { isr: true },
-    '/livestreams': { isr: true },
-    '/speaking': { isr: true },
-    '/presskit': { isr: true },
-    '/alive-and-kicking': { isr: true },
+  nitro: {
+    prerender: {
+      crawlLinks: true,
+      interval: 1000,
+      concurrency: 1
+    }
   },
+
+  // routeRules: {
+  //   '/': { isr: true },
+  //   '/about': { isr: true },
+  //   '/videos': { isr: true },
+  //   '/writing': { isr: true },
+  //   '/livestreams': { isr: true },
+  //   '/speaking': { isr: true },
+  //   '/presskit': { isr: true },
+  //   '/alive-and-kicking': { isr: true },
+  // },
 
   modules: [
     '@nuxtjs/tailwindcss',
