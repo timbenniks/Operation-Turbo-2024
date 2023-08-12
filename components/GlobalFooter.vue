1584e60e2c8be33a7e21bf1b388370fcbd431899
Tim Benniks <tbenniks@gmail.com>
2023-08-12 11:34:23 +0200

    JS for navigation

--- components/GlobalFooter.vue
+++ components/GlobalFooter.vue
@@ -7,7 +7,7 @@ const { pages } = await GqlPages();
       class="flex flex-col space-y-6 md:space-y-0 md:flex-row md:space-x-6 font-bold text-md items-center"
     >
       <li><tim /></li>
-      <li v-for="page in pages" :key="page.slug">
+      <li v-for="page in pages" :key="page.id">
         <nuxt-link :to="`${page.slug == 'home' ? '/' : `/${page.slug}`}`">{{
           page.navigationLabel
         }}</nuxt-link>
--- components/navigation.vue
+++ components/navigation.vue
@@ -1,48 +1,54 @@
 <script setup lang="ts">
-const open = ref(false);
+const { pages } = await GqlPages();
 </script>
 <template>
   <button
-    class="relative h-12 w-12 bg-white lg:hidden mt-6"
-    v-on:click="open = !open"
+    class="nav-toggle relative h-12 w-12 bg-white lg:hidden mt-6"
+    onclick="this.classList.toggle('open')"
   >
     <span class="sr-only">Open main menu</span>
     <div
       class="absolute left-1/2 top-1/2 block w-6 -translate-x-1/2 -translate-y-1/2 transform"
     >
       <span
-        class="absolute block h-0.5 w-6 transform bg-black transition duration-500 ease-in-out"
-        :class="open ? 'rotate-45' : '-translate-y-1.5'"
+        class="line-1 -translate-y-1.5 absolute block h-0.5 w-6 transform bg-black transition duration-500 ease-in-out"
       ></span>
       <span
-        class="absolute block h-0.5 w-6 transform bg-black transition duration-500 ease-in-out"
-        :class="open ? 'opacity-0' : ''"
+        class="line-2 absolute block h-0.5 w-6 transform bg-black transition duration-500 ease-in-out"
       ></span>
       <span
-        class="absolute block h-0.5 w-6 transform bg-black transition duration-500 ease-in-out"
-        :class="open ? '-rotate-45' : 'translate-y-1.5'"
+        class="line-3 translate-y-1.5 absolute block h-0.5 w-6 transform bg-black transition duration-500 ease-in-out"
       ></span>
     </div>
   </button>
   <ul
-    :class="open ? 'block' : 'hidden'"
-    class="mt-8 lg:mt-28 font-bold text-md space-y-2 navigation lg:block"
+    class="hidden mt-8 lg:mt-28 font-bold text-md space-y-2 navigation lg:block"
   >
     <li class="mb-4 hidden lg:block">
       <nuxt-link to="/" rel="noopener" title="Tim's Logo"><tim /></nuxt-link>
     </li>
-    <li><nuxt-link to="/about" rel="noopener">About</nuxt-link></li>
-    <li><nuxt-link to="/videos" rel="noopener">Videos</nuxt-link></li>
-    <li>
-      <nuxt-link to="/livestreams" rel="noopener">Live streams</nuxt-link>
+    <li v-for="page in pages" :key="page.id">
+      <nuxt-link :to="`${page.slug == 'home' ? '/' : `/${page.slug}`}`">{{
+        page.navigationLabel
+      }}</nuxt-link>
     </li>
-    <li><nuxt-link to="/writing" rel="noopener">Writing</nuxt-link></li>
-    <li><nuxt-link to="/speaking" rel="noopener">Speaking</nuxt-link></li>
-    <li>
-      <nuxt-link to="/alive-and-kicking" rel="noopener"
-        >Alive and Kicking</nuxt-link
-      >
-    </li>
-    <li><nuxt-link to="/presskit" rel="noopener">Press kit</nuxt-link></li>
   </ul>
 </template>
+
+<style lang="postcss" scoped>
+.nav-toggle.open + ul {
+  @apply block;
+}
+
+.nav-toggle.open .line-1 {
+  @apply rotate-45 translate-y-0;
+}
+
+.nav-toggle.open .line-2 {
+  @apply opacity-0;
+}
+
+.nav-toggle.open .line-3 {
+  @apply -rotate-45 translate-y-0;
+}
+</style>
